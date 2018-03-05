1256e3aed0db49bd2543079caebeeccd6dc83a53
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-03-05 08:39:13 -0800

    add beta version of material UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Error404 from './Error404';
 import Welcome from './Welcome';
 
